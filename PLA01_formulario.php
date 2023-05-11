2275e2b2c161200991608a33b5c363f4cc9aa871
andayanda <61781747+andayanda@users.noreply.github.com>
2023-05-11 16:07:47 +0200

    subidos de local

---
+++ PLA01_formulario.php
@@ -0,0 +1,40 @@
+<?php
+//recuperar variables de sesion si esta existe
+session_start();
+if (isset($_SESSION['datos'])){
+//crear variables independientes a partir de las claves asociativas del array 
+extract($_SESSION['datos']);
+}
+//echo "$nif $nom $cognom";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PLA01</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<div class='container'>
+		<h1 class='centrar'>PLA01: FORMULARI</h1>
+		<form method="post" action="PLA01_mostrardatos.php">
+			<label for='nif'>Nif</label>
+			<input type="text" name="nif" id='nif' value='<?php echo $nif ??null;?>'><br><br>
+			<label for='nombre'>Nom</label>
+			<input type="text" name="nom" id='nom' value='<?php echo $nom ??null;?>'><br><br>
+			<label for='apellidos'>Gognoms</label>
+			<input type="text" name="cognom" id='cognom' value='<?php echo $cognom ??null;?>'><br><br>
+			<label for='email'>Email</label>
+			<input type="email" name="email" id='email' value='<?php echo $email ??null;?>'><br><br>
+			<label for='nota'>Nota exàmen</label>
+			<input type="number" name="nota" id='nota' value='<?php echo $nota ??null;?>'><br><br>
+			
+			<label for='mensaje'>Missatge</label>
+			<textarea name='mensaje' id='mensaje' cols='22' rows='5'></textarea><br><br>
+			<label></label>
+			<input type="submit" name="Enviar">
+		</form>
+	</div>
+</body>
+</html>
---
+++ PLA01_mostrardatos.php
@@ -0,0 +1,105 @@
+<?php
+//activar variables de sesion
+session_start();
+$nif = trim(isset($_POST['nif']))?$_POST['nif']:null;
+$nom = trim($_POST['nom']??null);
+$cognom = trim($_POST['cognom']?? null);
+$nota = trim($_POST['nota']?? null);
+$email = trim(filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL));
+$errores = '';
+
+try {
+	//code...
+
+if (empty($nif)) {
+	$errores .= 'nif obligatori<br>';	
+}
+if (empty($nom)) {
+	$errores .=  'nom obligatori<br>';
+}
+if (empty($cognom)) {
+	$errores .=  'cognom obligatori<br>';
+}
+if (empty($email)) {
+	$errores .= 'email obligatori<br>';
+}
+if ($nota=='') {
+	$errores .=  'Nota obligatoria<br>';
+}
+	else {
+		if ($nota <0 || $nota >10) {
+
+			$errores .=  'Nota no permitida<br>';
+			
+		}
+	}
+
+if (!empty($errores)) {
+	throw new Exception($errores);
+	
+}
+// operativa que depende de la validación
+// evaluar la nota
+/*
+entre 0 < 5 = Supenso
+entre 5 y < 6 = aprobado
+entre 6 < 7 = bien
+entre 7 < 9 = notable
+entre 9 o mayor = excelente */
+if ($nota <5) {
+	$evaluacion = 'Supenso';
+	
+}
+elseif ($nota >= 5 && $nota <6) {
+	$evaluacion = 'Aprobado';	
+	$class= "rojo";
+}
+elseif ($nota >= 6 && $nota <7) {
+	$evaluacion = 'Bien';	
+	$class= "amarillo";
+}
+elseif ($nota >= 7 && $nota <9) {
+	$evaluacion = 'Notable';	
+	$class= "verde";
+}
+else $evaluacion = 'Excelente';
+$class= "azul";
+
+} catch (Exception $error) {
+	$mensajes = $error ->getMessage();
+} 
+//compactar datos en la variable para conservarlos y recuperarlos fuera de este formulario
+	$datos = compact('nif','nom', 'cognom', 'nota', 'email');
+	//echo '<pre>';print_r($datos);echo '</pre>';
+	$_SESSION ['datos'] = $datos;
+	 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PLA01</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css?v=1.0">
+</head>
+<body>
+	<div class='container'>
+		<h1 class='centrar'>PLA01: MOSTRAR DADES</h1>
+		<div class='card'>
+			<input type="text" name="nif" placeholder="nif" disabled value='<?php echo $nif;?>'><?php $nif ?><br><br>
+			<input type="text" name="nom" placeholder="nom" disabled value='<?php echo $nom;?>'>
+			<input type="text" name="cognom" placeholder="cognoms" disabled value='<?php echo $cognom;?>'><br><br>
+			<input type="text" name="qualificacio" placeholder="qualificació" disabled value='<?php echo $evaluacion ??null;?>'>
+			<!--aqui iran las cajitas <aside></aside>-->
+			
+			
+			<aside class="<?php echo $class ?>"></aside>
+			
+			<br><br>
+			<input type="text" placeholder="email" disabled value='<?php echo $email ??null;?>'><br><br>
+			<textarea  cols='22' rows='5' disabled></textarea>
+			<p class='errores'><?php echo $mensajes ??null; ?> </p>
+			<a href="PLA01_formulario.php">Volver al Formulario</a>
+		</div>
+	</div>
+</body>
+</html>
